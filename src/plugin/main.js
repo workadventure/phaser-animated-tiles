workadventure/phaser-animated-tiles

commit ff2abf022bc671868711152b91a6ad57a0583492

Set correct initial if animation starts with different tile than the one with animation flag

src/plugin/main.js
@@ -280,6 +280,8 @@ class AnimatedTiles extends Phaser.Plugins.ScenePlugin {
                                 });
                             // time until jumping to next frame
                             animatedTileData.next = animatedTileData.frames[0].duration;
+                            // set correct currentFrame if animation starts with different tile than the one with animation flag
+                            animatedTileData.currentFrame = animatedTileData.frames.findIndex(f => f.tileid === index + tileset.firstgid);
                             // Go through all layers for tiles
                             map.layers.forEach(
                                 (layer) => {

commit 2d5c66a9bc426dd4cb2d856c1d599494a74f8067

plugin: handle broken firtgid gracefuly
It seems like change introduced in
ff2abf022bc671868711152b91a6ad57a0583492 breaks *some* maps we used at
rc3. This should gracefuly revert back to "old" behaviour.

src/plugin/main.js
@@ -280,8 +280,13 @@ class AnimatedTiles extends Phaser.Plugins.ScenePlugin {
                                 });
                             // time until jumping to next frame
                             animatedTileData.next = animatedTileData.frames[0].duration;
+
                             // set correct currentFrame if animation starts with different tile than the one with animation flag
-                            animatedTileData.currentFrame = animatedTileData.frames.findIndex(f => f.tileid === index + tileset.firstgid);
+                            const firstFrame = animatedTileData.frames.findIndex(f => f.tileid === index + tileset.firstgid);
+                            if (firstFrame !== -1) {
+                              animatedTileData.currentFrame = firstFrame;
+                            }
+
                             // Go through all layers for tiles
                             map.layers.forEach(
                                 (layer) => {